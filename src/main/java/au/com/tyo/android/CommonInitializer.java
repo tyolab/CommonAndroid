e5882ffcd843576c752f76e71c8408bf2a029c15
Fee Mon <monfee@Fees-MacBook-Pro.local>
2017-01-13 13:25:29 +1000

    initial import

---
+++ src/main/java/au/com/tyo/android/CommonInitializer.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2015 TYONLINE TECHNOLOGY PTY. LTD. (TYO Lab)
+ *
+ */
+
+package au.com.tyo.android;
+
+
+/**
+ * @author Eric Tang <eric.tang@tyo.com.au>
+ */
+
+
+public class CommonInitializer {
+	
+	public static Class clsController = null;
+	
+	/**
+	 * let CommonApp class to deal with empty ui class
+	 */
+	public static Class clsUi = null; // UIBase.class;
+	
+	//public static Class clsData; // normally we don't have this
+
+}
