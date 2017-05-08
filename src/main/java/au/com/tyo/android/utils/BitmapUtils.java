31146edb2401ed4209aa56da52af9ed6b127cd17
tyodev <deve@tyo.com.au>
2017-05-08 18:48:01 +1000

    getting better

--- src/main/java/au/com/tyo/android/utils/BitmapUtils.java
+++ src/main/java/au/com/tyo/android/utils/BitmapUtils.java
@@ -16,15 +16,19 @@
 
 package au.com.tyo.android.utils;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.util.Log;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.util.Log;
 
 public class BitmapUtils {
 	
@@ -60,5 +64,45 @@ public class BitmapUtils {
 	
 	public static Bitmap inputStreamToBitmap(InputStream stream) {
 		return BitmapFactory.decodeStream(stream);
+	}
+
+	/**
+	 * http://stackoverflow.com/questions/8381514/android-converting-color-image-to-grayscale
+	 *
+	 * @param bmpOriginal
+	 * @return
+	 */
+	public Bitmap toGrayscale(Bitmap bmpOriginal) {
+        /*
+        similarly
+        for(int x = 0; x < width; ++x) {
+            for(int y = 0; y < height; ++y) {
+                // get one pixel color
+                pixel = src.getPixel(x, y);
+                // retrieve color of all channels
+                A = Color.alpha(pixel);
+                R = Color.red(pixel);
+                G = Color.green(pixel);
+                B = Color.blue(pixel);
+                // take conversion up to one single value
+                R = G = B = (int)(0.299 * R + 0.587 * G + 0.114 * B);
+                // set new pixel color to output bitmap
+                bmOut.setPixel(x, y, Color.argb(A, R, G, B));
+            }
+        }
+         */
+		int width, height;
+		height = bmpOriginal.getHeight();
+		width = bmpOriginal.getWidth();
+
+		Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+		Canvas c = new Canvas(bmpGrayscale);
+		Paint paint = new Paint();
+		ColorMatrix cm = new ColorMatrix();
+		cm.setSaturation(0);
+		ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+		paint.setColorFilter(f);
+		c.drawBitmap(bmpOriginal, 0, 0, paint);
+		return bmpGrayscale;
 	}
 }
