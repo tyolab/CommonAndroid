a8853cd80fa2c9948ec35eacf75e0e76e20bc264
tyodev <deve@tyo.com.au>
2017-07-07 12:43:28 +1000

    added new svg handling library

--- src/main/java/au/com/tyo/android/services/ImageDownloader.java
+++ src/main/java/au/com/tyo/android/services/ImageDownloader.java
@@ -3,9 +3,7 @@ package au.com.tyo.android.services;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.graphics.drawable.PictureDrawable;
 import android.widget.ImageView;
 
 import java.io.File;
@@ -17,82 +15,77 @@ import au.com.tyo.android.images.svg.SVGBuilder;
 import au.com.tyo.android.images.utils.BitmapUtils;
 import au.com.tyo.data.ContentTypes;
 
-public class ImageDownloader extends ResourceFetcher<Drawable, ImageView> {
-	
-	private static final String LOG_TAG = "ImageDownloader";
+public class ImageDownloader extends ResourceFetcher<Bitmap, ImageView> {
 
-	private int quality = 80;
+    private static final String LOG_TAG = "ImageDownloader";
 
-	private int sampleSize = 2;
+    private int quality = 80;
 
-	public int getSampleSize() {
-		return sampleSize;
-	}
+    private int sampleSize = 2;
 
-	public void setSampleSize(int sampleSize) {
-		this.sampleSize = sampleSize;
-	}
+    public int getSampleSize() {
+        return sampleSize;
+    }
 
-	public ImageDownloader(Context context, String subdir){
-		super(context, subdir);
-	}
+    public void setSampleSize(int sampleSize) {
+        this.sampleSize = sampleSize;
+    }
 
-	@Override
-	public Drawable read(File f) {
-		return new BitmapDrawable(getContext().getResources(), BitmapFactory.decodeFile(f.getPath()));
-	}
+    public ImageDownloader(Context context, String subdir){
+        super(context, subdir);
+    }
 
-	public int getQuality() {
-		return quality;
-	}
+    @Override
+    public Bitmap read(File f) {
+        return BitmapFactory.decodeFile(f.getPath());
+    }
 
-	public void setQuality(int quality) {
-		this.quality = quality;
-	}
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
 
 
-	///////////////////////
-	@Override
-	public void write(Drawable target, File f) {
-		FileOutputStream out = null;
+    ///////////////////////
+    @Override
+    public void write(Bitmap target, File f) {
+        FileOutputStream out = null;
 
-		try {
-			out = new FileOutputStream(f);
-            Bitmap bitmap = BitmapUtils.drawableToBitmap((PictureDrawable) target);
-			bitmap.compress(Bitmap.CompressFormat.PNG, quality, out);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		finally {
-			try { if (out != null ) out.close(); }
-			catch(Exception ex) {} 
-		}
-	}
+        try {
+            out = new FileOutputStream(f);
+            target.compress(Bitmap.CompressFormat.PNG, quality, out);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            try { if (out != null ) out.close(); }
+            catch(Exception ex) {}
+        }
+    }
 
-	@Override
-	protected Drawable processInputStream(InputStream inputStream, String url) {
-		if (ContentTypes.isSVG(url)) {
+    @Override
+    protected Bitmap processInputStream(InputStream inputStream, String url) {
+        if (ContentTypes.isSVG(url)) {
             SVG svg = new SVGBuilder()
                     .readFromInputStream(inputStream)
                     .build();
 
             Drawable drawable = svg.getDrawable();
-            return drawable;
-		}
+            return BitmapUtils.drawableToBitmap(drawable);
+        }
 
         final BitmapFactory.Options options = new BitmapFactory.Options();
         options.inSampleSize = sampleSize;
-        Bitmap bitmap = BitmapFactory.decodeStream(inputStream, null, options);
-		return new BitmapDrawable(getContext().getResources(), bitmap);
-	}
+        return BitmapFactory.decodeStream(inputStream, null, options);
+    }
 
     @Override
-    public void handleResult(ImageView container, Drawable file) {
+    public void handleResult(ImageView container, Bitmap file) {
         if (null != container && file != null) {
-            // container.setImageBitmap(file);
-            //
-            Bitmap bitmap = BitmapUtils.drawableToBitmap(file);
-            container.setImageBitmap(bitmap);
+            container.setImageBitmap(file);
         }
 
         /**
