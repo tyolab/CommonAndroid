d8c93ca2e91267385ad297db04df2239760cf53f
tyodev <deve@tyo.com.au>
2018-01-31 17:38:47 +1000

    added ic_action_go button

--- src/main/java/au/com/tyo/android/AndroidUtils.java
+++ src/main/java/au/com/tyo/android/AndroidUtils.java
@@ -698,4 +698,36 @@ public class AndroidUtils {
 		}
 		return result;
 	}
+
+	private static String getSystemProperty(String name) {
+		try {
+			Class systemPropertyClass = Class.forName("android.os.SystemProperties");
+			return (String) systemPropertyClass.getMethod("get", new Class[]{String.class}).invoke(systemPropertyClass, new Object[]{name});
+		}
+		catch (Exception ex) {
+		}
+		return "";
+	}
+
+	/*
+	// Build.FINGERPRINT.startsWith("generic")
+	//            || Build.FINGERPRINT.startsWith("unknown")
+	//					|| Build.MODEL.contains("google_sdk")
+	//					|| Build.MODEL.contains("Emulator")
+	//					|| Build.MODEL.contains("Android SDK built for x86")
+	//					|| Build.MANUFACTURER.contains("Genymotion")
+	//					|| (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+	//					|| "google_sdk".equals(Build.PRODUCT);
+	*/
+	/**
+	 *
+	 *
+	 * @return
+	 */
+	public static boolean isEmulator() {
+		boolean goldfish = getSystemProperty("ro.hardware").contains("goldfish");
+		boolean emu = getSystemProperty("ro.kernel.qemu").length() > 0;
+		boolean sdk = getSystemProperty("ro.product.model").equals("sdk");
+		return goldfish || emu || sdk;
+	}
 }
