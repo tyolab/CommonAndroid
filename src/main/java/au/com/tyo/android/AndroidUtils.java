e249e8445e26961c3905f90438304ebbaa8c477c
Eric Tang <eric.tang@tyo.com.au>
2018-09-28 21:32:46 +1000

    the merging works now

--- src/main/java/au/com/tyo/android/AndroidUtils.java
+++ src/main/java/au/com/tyo/android/AndroidUtils.java
@@ -39,6 +39,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.FrameLayout;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
 import java.io.Serializable;
 import java.lang.reflect.Field;
@@ -864,5 +865,52 @@ public class AndroidUtils {
 		WallpaperManager wallpaperManager
 				= WallpaperManager.getInstance(context);
 		wallpaperManager.setResource(resourceId);
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	public static int getNumberOfCpuCores() {
+		if(Build.VERSION.SDK_INT >= 17) {
+			return Runtime.getRuntime().availableProcessors();
+		}
+		else {
+			// Use saurabh64's answer
+			return getNumCoresOldPhones();
+		}
+	}
+
+	/**
+	 * @link{https://stackoverflow.com/questions/30119604/how-to-get-the-number-of-cores-of-an-android-device}
+	 *
+	 * Gets the number of cores available in this device, across all processors.
+	 * Requires: Ability to peruse the filesystem at "/sys/devices/system/cpu"
+	 * @return The number of cores, or 1 if failed to get result
+	 */
+	private static int getNumCoresOldPhones() {
+		//Private Class to display only CPU devices in the directory listing
+		class CpuFilter implements FileFilter {
+			@Override
+			public boolean accept(File pathname) {
+				//Check if filename is "cpu", followed by a single digit number
+				if(Pattern.matches("cpu[0-9]+", pathname.getName())) {
+					return true;
+				}
+				return false;
+			}
+		}
+
+		try {
+			//Get directory containing CPU info
+			File dir = new File("/sys/devices/system/cpu/");
+			//Filter to only list the devices we care about
+			File[] files = dir.listFiles(new CpuFilter());
+			//Return the number of cores (virtual CPU devices)
+			return files.length;
+		} catch(Exception e) {
+			//Default to return 1 core
+			return 1;
+		}
 	}
 }
